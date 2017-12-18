6599db9204abf4e8822e93f66132186e08c88578
Kevin Yang <yangdongkevin@gmail.com>
2017-12-18 11:40:51 -0800

    Update Carousel.tsx

--- src/Carousel.tsx
+++ src/Carousel.tsx
@@ -181,7 +181,7 @@ export default class Carousel extends React.Component<CarouselProps, CarouselSta
         this.gotoPage(Math.floor(this.currentIndex) + 1);
     }
 
-    private gotoPage(index: number, animated: boolean = true, cb = () => { }) {
+    public gotoPage(index: number, animated: boolean = true, cb = () => { }) {
         const childrenNum = this.getChildrenNum();
         if (childrenNum <= 1) {
             return cb();
